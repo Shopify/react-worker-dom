dec929ab27a11cbcdee6db5b624689b86e5223dd
Gianni Chiappetta <gianni@runlevel6.org>
2017-03-14 17:45:27 -0400

    Tweak module resolution roots

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,42 @@
-var path = require('path');
-var webpack = require('webpack');
+const { resolve } = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-    devtool: 'cheap-module-source-map',
-    context: __dirname,
-    entry: {
-        'worker': './src/worker/index.js',
-        'page': './src/page/index.js',
-    },
-    output: {
-        filename: '[name].js',
-        path: __dirname,
-        publicPath: '/static/',
-        library: 'react-worker-dom',
-        libraryTarget: 'umd',
-    },
-    module: {
-        loaders: [{
-            test: /\.js?$/,
-            exclude: /node_modules/,
-            loader: 'babel',
-            query: {
-                plugins: ['transform-decorators-legacy'],
-                presets: ['es2015', 'react', 'stage-0']
-            }
-        }]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-          'process.env': {
-            'NODE_ENV': JSON.stringify('production')
-          },
-        })
+  devtool: 'source-map',
+  context: __dirname,
+  entry: {
+    'worker': './src/worker/index.js',
+    'page': './src/page/index.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        plugins: ['transform-decorators-legacy'],
+        presets: ['es2015', 'react', 'stage-0']
+      }
+    }]
+  },
+  output: {
+    filename: '[name].js',
+    path: __dirname,
+    publicPath: '/static/',
+    library: 'react-worker-dom',
+    libraryTarget: 'umd',
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      },
+    })
   ],
+  resolve: {
+    modules: [
+      resolve(__dirname, './src'),
+      resolve(__dirname, './node_modules')
+    ]
+  },
 };
